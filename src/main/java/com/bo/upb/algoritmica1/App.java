e20312be9567634b9f26c468ff089d24aca81900
Isabela <Isabela@mail.coma>
2023-05-10 10:25:43 -0400

    Initial commit

---
+++ src/main/java/com/bo/upb/algoritmica1/App.java
@@ -0,0 +1,16 @@
+package com.bo.upb.algoritmica1;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+}
